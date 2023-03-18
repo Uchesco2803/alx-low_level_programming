119a7a47903a6a78279a9a7d8759cf13392b85b0
Uchesco2803 <saintsixtus2000@gmail.com>
2023-03-18 14:35:49 -0700

    Fourth commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main-This is the main fuction description
+ *
+ *Return: 0 for succesful exit
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main-This is the main function description
+ *
+ * Return: 0 for successful exit
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
